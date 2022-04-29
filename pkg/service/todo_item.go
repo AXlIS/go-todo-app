467895f101853f9a85c4b01c59bb31eb04a64f90
Igor Svistun <igorekha2003@icloud.com>
2022-04-29 14:13:00 +0300

    Add handlers for task items

---
+++ pkg/service/todo_item.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"github.com/AXlIS/go-todo-app"
+	"github.com/AXlIS/go-todo-app/pkg/repository"
+)
+
+type TodoItemService struct {
+	repos     repository.TodoItem
+	listRepos repository.TodoList
+}
+
+func NewTodoItemService(repos repository.TodoItem, listRepos repository.TodoList) *TodoItemService {
+	return &TodoItemService{
+		repos:     repos,
+		listRepos: listRepos,
+	}
+}
+
+func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, error) {
+	_, err := s.listRepos.GetById(userId, listId)
+	if err != nil {
+		// list does not exist or does not belong to user
+		return 0, err
+	}
+	return s.repos.Create(listId, item)
+}
+
+func (s *TodoItemService) GetAll(userId, listId int) ([]todo.TodoItem, error) {
+	return s.repos.GetAll(userId, listId)
+}
+
+func (s *TodoItemService) GetById(userId, itemId int) (todo.TodoItem, error) {
+	return s.repos.GetById(userId, itemId)
+}
+
+//
+//func (s *TodoItemService)()  {
+//
+//}
+//
+//func (s *TodoItemService)()  {
+//
+//}
